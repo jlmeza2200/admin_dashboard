2a095cf15b56836450d1633814980d0a63012303
jonatan <jonatanmeza01@gmail.com>
2022-08-25 18:02:28 -0300

    Second commit

---
+++ order.js
@@ -0,0 +1,44 @@
+const Orders = [
+  {
+    productName: "Fodable Mini Done",
+    productNumber: "865452",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Ruko F11 Pro Drone",
+    productNumber: "856212",
+    paymentStatus: "Due",
+    shipping: "Declined",
+  },
+  {
+    productName: "Drone with Camera Drone",
+    productNumber: "36981",
+    paymentStatus: "Due",
+    shipping: "Deivered",
+  },
+  {
+    productName: "GPS 4K Drone",
+    productNumber: "12586",
+    paymentStatus: "Refunded",
+    shipping: "Deivered",
+  },
+  {
+    productName: "Larvender FK102 Drone",
+    productNumber: "400026",
+    paymentStatus: "Paid",
+    shipping: "Pending",
+  },
+  {
+    productName: "Fodable Mini Done",
+    productNumber: "39910",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Fodable Mini Done",
+    productNumber: "110087",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+];
